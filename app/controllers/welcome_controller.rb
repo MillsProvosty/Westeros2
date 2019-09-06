7a82c30e4c7ab16d8f77df35125e01b6993fb094
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-06 10:33:56 -0600

    Drop down menu functioning

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,17 @@
 class WelcomeController < ApplicationController
   def index
+    render locals: {
+      houses: houses
+    }
+  end
 
+  def houses
+  [
+    ['Stark', 'stark'],
+    ['Lannister', 'lannister'],
+    ['Targaryen', 'targaryen'],
+    ['Tyrell', 'tyrell'],
+    ['Greyjoy', 'greyjoy']
+    ]
   end
 end
