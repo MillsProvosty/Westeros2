1b990d99d04eac6bff8899871fcb55ca41d51727
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-06 11:12:10 -0600

    Adds Service

--- app/facades/search_westeros_facade.rb
+++ app/facades/search_westeros_facade.rb
@@ -1,18 +1,7 @@
 class SearchWesterosFacade
   def find_members(house)
-    this = get_json("/api/v1/house/#{house}?api_key=#{ENV["WESTEROS_API_KEY"]}")
-    binding.pry
+    WesterosService.new.retrieve(house)
   end
 
-  private
-    def connection
-      Faraday.new(:url => "http://westerosapi.herokuapp.com") do |f|
-        f.adapter  Faraday.default_adapter
-      end
-    end
-
-    def get_json(url)
-      response = connection.get(url)
-      JSON.parse(response.body)
-    end
+  
 end
