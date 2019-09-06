517d277f484f8a14249c1297dd02b7cd25937468
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-06 10:52:18 -0600

    search controller

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,13 @@
+class SearchController < ApplicationController
+  def index
+    render locals: {
+      facade: SearchWesterosFacade.new(params[:house])
+    }
+  end
+end
+
+# What is the code I want?
+# I want a service that will search the Westeros API
+# for the members of a specific house, which is being passed in.
+# That service will return a list of the members that I can iterate through on the view page.
+#
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,17 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    render locals: {
-      houses: houses
-    }
+
   end
 
-  def houses
-  [
-    ['Stark', 'stark'],
-    ['Lannister', 'lannister'],
-    ['Targaryen', 'targaryen'],
-    ['Tyrell', 'tyrell'],
-    ['Greyjoy', 'greyjoy']
-    ]
-  end
+
 end
